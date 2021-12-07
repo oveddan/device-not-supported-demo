36db408e1a5ae2a22930a798bf41e92a353e4533
Dan Oved <stangogh@gmail.com>
2021-12-06 16:17:08 -0800

    added device debug

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import { Device } from 'mediasoup-client';
 
 const deviceDebug = document.getElementsByTagName('h2')[0];
+const browserDebug = document.getElementsByTagName('p')[0];
+
+if (typeof navigator === 'object' && navigator.product === 'ReactNative')
+{
+  browserDebug.innerText = 'device: ReactNative';
+} else {
+  browserDebug.innerText = `user agent: ${navigator.userAgent}`;
+}
 
 // Create a device (use browser auto-detection).
 try {
-new Device();
+  new Device();
 
-deviceDebug.innerText = 'succeeeded loading device.';
+  deviceDebug.innerText = 'succeeeded loading device.';
   // deviceDebug.innerText = device.rtpCapabilities;
 } catch(e) {
   deviceDebug.innerText = e.toString();
